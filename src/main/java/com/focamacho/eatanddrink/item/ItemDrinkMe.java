2d4d71f804c3cfe16abbf8f63ce8c0375046901b
Focamacho <renancolombomail@gmail.com>
2020-07-31 19:51:13 -0300

    Now entities can be feed with "Eat Me" and "Drink Me"

--- src/main/java/com/focamacho/eatanddrink/item/ItemDrinkMe.java
+++ src/main/java/com/focamacho/eatanddrink/item/ItemDrinkMe.java
@@ -3,10 +3,15 @@ package com.focamacho.eatanddrink.item;
 import moriyashiine.sizeentityattributes.SizeEntityAttributes;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 
@@ -28,12 +33,36 @@ public class ItemDrinkMe extends Item {
     }
 
     @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.DRINK;
+    }
+
+    @Override
     public SoundEvent getEatSound() {
         return SoundEvents.ENTITY_GENERIC_DRINK;
     }
 
     @Override
-    public UseAction getUseAction(ItemStack stack) {
-        return UseAction.DRINK;
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        EntityAttributeInstance height = user.getAttributeInstance(SizeEntityAttributes.HEIGHT_MULTIPLIER);
+        EntityAttributeInstance width = user.getAttributeInstance(SizeEntityAttributes.WIDTH_MULTIPLIER);
+        if(height != null && width != null && height.getValue() != 0.125D) {
+            return super.use(world, user, hand);
+        }
+        return TypedActionResult.pass(user.getStackInHand(hand));
+    }
+
+    @Override
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        EntityAttributeInstance height = entity.getAttributeInstance(SizeEntityAttributes.HEIGHT_MULTIPLIER);
+        EntityAttributeInstance width = entity.getAttributeInstance(SizeEntityAttributes.WIDTH_MULTIPLIER);
+        if(height != null && width != null && height.getValue() != 0.125D) {
+            height.setBaseValue(height.getValue() - 0.4);
+            width.setBaseValue(width.getValue() - 0.4);
+            stack.decrement(1);
+            entity.world.playSound(null, entity.getX(), entity.getY(), entity.getZ(), this.getEatSound(), SoundCategory.NEUTRAL, 1.0F, 1.0F + (entity.world.random.nextFloat() - entity.world.random.nextFloat()) * 0.4F);
+            return ActionResult.CONSUME;
+        }
+        return ActionResult.FAIL;
     }
 }
--- src/main/java/com/focamacho/eatanddrink/item/ItemEatMe.java
+++ src/main/java/com/focamacho/eatanddrink/item/ItemEatMe.java
@@ -3,8 +3,13 @@ package com.focamacho.eatanddrink.item;
 import moriyashiine.sizeentityattributes.SizeEntityAttributes;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 public class ItemEatMe extends Item {
@@ -22,6 +27,30 @@ public class ItemEatMe extends Item {
             width.setBaseValue(width.getValue() + 0.4);
         }
         return super.finishUsing(stack, world, user);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        EntityAttributeInstance height = user.getAttributeInstance(SizeEntityAttributes.HEIGHT_MULTIPLIER);
+        EntityAttributeInstance width = user.getAttributeInstance(SizeEntityAttributes.WIDTH_MULTIPLIER);
+        if(height != null && width != null && height.getValue() != 8.0D) {
+            return super.use(world, user, hand);
+        }
+        return TypedActionResult.pass(user.getStackInHand(hand));
+    }
+
+    @Override
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        EntityAttributeInstance height = entity.getAttributeInstance(SizeEntityAttributes.HEIGHT_MULTIPLIER);
+        EntityAttributeInstance width = entity.getAttributeInstance(SizeEntityAttributes.WIDTH_MULTIPLIER);
+        if(height != null && width != null && height.getValue() != 8.0D) {
+            height.setBaseValue(height.getValue() + 0.4);
+            width.setBaseValue(width.getValue() + 0.4);
+            stack.decrement(1);
+            entity.world.playSound(null, entity.getX(), entity.getY(), entity.getZ(), this.getEatSound(), SoundCategory.NEUTRAL, 1.0F, 1.0F + (entity.world.random.nextFloat() - entity.world.random.nextFloat()) * 0.4F);
+            return ActionResult.CONSUME;
+        }
+        return ActionResult.FAIL;
     }
 
 }
